511b4b2f60d83c71a30c82e1154b566ac816e630
Dhruv Singh K <dhruvsinghk@Dhruvs-iMac.local>
2024-09-25 21:33:08 +0530

    hovering messages and menu bar from the left

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,58 @@
-import { IonAvatar, IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonInput, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonApp, IonAvatar, IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonList, IonMenu, IonMenuButton, IonMenuToggle, IonPage, IonPopover, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import { checkmark } from 'ionicons/icons';
+import { checkmark, menu } from 'ionicons/icons';
 
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-
+    <IonApp>
+    <IonMenu contentId="main">
       <IonHeader>
         <IonToolbar>
+          <IonTitle>Menu Content</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonMenuToggle>
+          <IonButton>Click to close the menu</IonButton>
+        </IonMenuToggle>
+      </IonContent>
+    </IonMenu>
+
+    <IonPage id="main">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuToggle autoHide={false}>
+              <IonButton>
+                <IonIcon slot="icon-only" icon={menu} />
+              </IonButton>
+            </IonMenuToggle>
+          </IonButtons>
           <IonTitle>Physical Count Page</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="relative">
-        <div className='w-full bg-gray-400 h-1/4 absolute rounded-lg'>
-          <IonInput className='absolute top-4 left-4' placeholder='Enter Count' labelPlacement='fixed'/>
-          <div className='absolute top-16 left-2 right-2 h-0.5 bg-'></div>
+        <div className='grid grid-rows-2'>
+          <div className='w-full bg-gray-400 rounded-lg relative p-4'>
+            <IonInput className='w-full' style={{ marginTop: '16px' }} placeholder='Enter Count' labelPlacement='fixed' />
+            <div className='mt-4 h-0.5 bg-gray-600'></div>
+          </div>
+          <div className='w-full bg-green-200 flex items-center justify-center'>
+            <IonButton id="click-trigger">Left-Click Me</IonButton>
+            <IonPopover trigger="click-trigger" triggerAction="click">
+              <IonContent className="ion-padding">{`ij`}</IonContent>
+            </IonPopover>
+          </div>
         </div>
-        <IonButton shape='round' className='absolute bottom-4 left-4' expand="block" >
+        <IonButton shape='round' className='absolute bottom-4 left-4' expand="block">
           <IonIcon slot='icon-only' icon={checkmark}></IonIcon>
         </IonButton>
       </IonContent>
     </IonPage>
+  </IonApp>
+
   );
 };
 
