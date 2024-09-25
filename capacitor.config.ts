2002ce5e8a58a0c9e316b45a85b7a1015fb5d5d0
Dhruv Singh K <dhruvsinghk@Dhruvs-iMac.local>
2024-09-25 10:32:40 +0530

    commit for appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic-experiment',
   appName: 'ionic-react-tailwind',
   webDir: 'dist'
 };
+
+ios: { scheme: "App" }
 
 export default config;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonInput, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { checkmark } from 'ionicons/icons';
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
+
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Physical Count Page</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="relative">
+        <div className='w-full bg-gray-400 h-1/4 absolute rounded-lg'>
+          <IonInput className='absolute top-4 left-4' placeholder='Enter Count' labelPlacement='fixed'/>
+          <div className='absolute top-16 left-2 right-2 h-0.5 bg-'></div>
+        </div>
+        <IonButton shape='round' className='absolute bottom-4 left-4' expand="block" >
+          <IonIcon slot='icon-only' icon={checkmark}></IonIcon>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
